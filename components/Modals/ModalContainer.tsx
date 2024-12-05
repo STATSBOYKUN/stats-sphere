60e0dd3fff6faec1d635629a50548ef9b345a55c
HIMAWAN WAHID IKHWANSYAH <222112094@stis.ac.id>
2024-12-05 16:58:51 +0700

    Update ModalContainer.tsx

--- components/Modals/ModalContainer.tsx
+++ components/Modals/ModalContainer.tsx
@@ -4,14 +4,10 @@
 
 import React from 'react';
 import { useModal, ModalType } from '@/hooks/useModal';
-import OpenFileModal from './OpenFileModal';
-import SaveFileModal from './SaveFileModal';
-import ComputeVariableModal from "@/components/Modals/ComputeVariableModal";
 import SmoothingModal from '@/components/Modals/Analyze/TimeSeries/SmoothingModal';
 import DecompositionModal from '@/components/Modals/Analyze/TimeSeries/DecompositionModal';
 import StationaryTestModal from '@/components/Modals/Analyze/TimeSeries/StationaryTestModal';
 import CreateModelModal from '@/components/Modals/Analyze/TimeSeries/CreateModelModal';
-import ExportDataModal from './ExportDataModal';
 import OpenFileModal from './File/OpenFileModal';
 import SaveFileModal from './File/SaveFileModal';
 import ComputeVariableModal from "@/components/Modals/Transform/ComputeVariableModal";
