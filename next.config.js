6b6c1b766ae3f0b0bcf6a81b36b87c101f1c8d78
GuswanaAdv <222112082@stis.ac.id>
2024-12-23 18:01:08 +0700

    menambahkan wasm

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+const path = require("path");
 
-module.exports = nextConfig
+module.exports = {
+        nextConfig, 
+        webpack: (config) => {
+        config.resolve.alias["wasm"] = path.resolve(__dirname, "src/wasm/pkg");
+        return config;
+    },
+};
